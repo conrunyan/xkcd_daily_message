ff5ca5ba06b70388775c2cd249d4eb8c0766a03e
connor <connor.runyan@aggiemail.usu.edu>
2018-05-04 03:30:09 +0000

    Added outlineof cron_job

---
+++ cron_job.sh
@@ -0,0 +1,7 @@
+set -e
+set -x
+
+# TODO: call get_comic.sh
+# TODO:     - call parse_json.py (returns url of comic)
+#           - calls a wget on the comic url in the JSON data
+# TODO: send email to me with xkcd comic as attachment
