d8f90d3c0aea8e18f425f07b0738fc7c2d17eb65
connor <connor.runyan@aggiemail.usu.edu>
2018-05-04 03:23:52 +0000

    Added steps to get the newest xkcd json

--- get_comic.sh
+++ get_comic.sh
@@ -1,9 +1,14 @@
+#!/bin/bash
 set -e
+set -x
 
 comic_name=$1
+new_comic_dir="/home/connor/projects/xkcd_term/new_comic/"
 # get daily comic
 xkcd_url="https://xkcd.com/info.0.json"
-wget $xkcd_url > $1
+
+wget -O $1 $xkcd_url
 # delete old comic (for testing move to hold_comic)
-rm 
-# move comic to new_comic dir
+rm -f $new_comic_dir*
+# move comic json to new_comic dir
+mv $1 $new_comic_dir
