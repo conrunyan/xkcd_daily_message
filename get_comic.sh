a9de28ea9240f4ef04e6e88bb692b05d91ec00b7
connor <connor.runyan@aggiemail.usu.edu>
2018-05-07 05:32:41 +0000

    Fixed grep. needed to add -o option

--- get_comic.sh
+++ get_comic.sh
@@ -6,8 +6,7 @@ new_comic_dir="/home/connor/projects/xkcd_term/new_comic/"
 # get daily comic
 xkcd_url="https://xkcd.com/info.0.json"
 img_url=`curl -s "$xkcd_url" | python -c "import json, sys; print (json.load(sys.stdin)['img'])"`
-img_name=echo $img_url | grep -E '\w+.png$'
-echo $img_url
+img_name=`echo $img_url | grep -Eo "\w+.png\$"`
 wget -O $img_name $img_url
 # delete old comic (for testing move to hold_comic)
 rm -f $new_comic_dir*
