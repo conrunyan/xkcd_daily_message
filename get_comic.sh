bcdc5e2e95ccf389a065c06c5755bb9f847351f4
connor <connor.runyan@aggiemail.usu.edu>
2018-05-07 05:13:52 +0000

    Changed way the get_comic script works. Uses curl & python, along with wget now

--- get_comic.sh
+++ get_comic.sh
@@ -2,13 +2,14 @@
 set -e
 set -x
 
-comic_name=$1
 new_comic_dir="/home/connor/projects/xkcd_term/new_comic/"
 # get daily comic
 xkcd_url="https://xkcd.com/info.0.json"
-
-wget -O $1 $xkcd_url
+img_url=`curl -s "$xkcd_url" | python -c "import json, sys; print (json.load(sys.stdin)['img'])"`
+img_name=echo $img_url | grep -E '\w+.png$'
+echo $img_url
+wget -O $img_name $img_url
 # delete old comic (for testing move to hold_comic)
 rm -f $new_comic_dir*
-# move comic json to new_comic dir
+# move comic to new_comic dir
 mv $1 $new_comic_dir
